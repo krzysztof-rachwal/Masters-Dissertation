48f063bcf17dc0df1419aed3a1d6981ea40efbd6
Daniel <potkanskid@cardiff.ac.uk>
2020-07-06 13:45:51 +0100

    initial commit

---
+++ src/test/java/com/example/edubizexchange/EdubizexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edubizexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdubizexchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
