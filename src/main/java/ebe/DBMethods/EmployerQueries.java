7d2e7d20c47368bf5fafde9548d185b0cff77253
Daniel Leite <leiteDF@cardiff.ac.uk>
2020-07-20 15:30:06 +0100

    Create get employer information by Id funciton

--- src/main/java/ebe/DBMethods/EmployerQueries.java
+++ src/main/java/ebe/DBMethods/EmployerQueries.java
@@ -32,7 +32,27 @@ public class EmployerQueries extends DBQueries {
                 );
     }
 
+    // 2. Get Employer by Id
+    public Employer getEmployerDetailsById(int employerId) throws DataAccessException{
+        String getSql = String.format("SELECT * FROM Employer WHERE EmployerID = \"%s\" LIMIT 1", employerId);
+        List<Employer>  employerInfo = jdbcTemplate().query(getSql, new Object[]{},
+                (rs, i) -> new Employer(rs.getInt("EmployerID"), rs.getInt("StatusOfEmployer"),
+                        rs.getString("EmployerName"), rs.getString("EmployerAddressCity"),
+                        rs.getString("EmployerAddressStreet"),rs.getString("EmployerAddressNumber"),
+                        rs.getString("EmployerPostCode"),rs.getString("Email"), rs.getString("Phone"),
+                        rs.getString("Website"), rs.getInt("NumberOfEmployees"), rs.getString("CompanySummary"),
+                        rs.getString("Notes"), rs.getString("EmployerDocumentsAndVideos"), rs.getString("EmployerLogo"),
+                        rs.getBoolean("GivesSiteExperience"),rs.getBoolean("GivesSiteVisits"), rs.getBoolean("GivesWorkshops") ,
+                        rs.getBoolean("GivesPresentations"),rs.getBoolean("AttendsCareerFairs"), rs.getBoolean("GivesWebinars"),
+                        rs.getBoolean("WorksWithPrimaryPupils"),rs.getBoolean("UseOfModernForeignLanguage"),
+                        rs.getBoolean("RunsBusinessInWelsh"), rs.getBoolean("CanDeliverToSchoolsInWelsh"),
+                        rs.getBoolean("HasApprenticeshipProgramm"),rs.getInt("SchoolsPreferences"),rs.getString("Employerscol"))
+        );
+        return employerInfo.get(0);
+    }
+
     ///////////////////////////////////// UPDATE ALL METHODS ///////////////////////////////////////////////
+
 
     ///////////////////////////////////// DELETE ALL METHODS ///////////////////////////////////////////////
 }
