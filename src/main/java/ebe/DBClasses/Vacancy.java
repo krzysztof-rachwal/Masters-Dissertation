c7f80f7d1fa12eb48e146ae76f72ed6ce1db6241
Daniel Leite <leiteDF@cardiff.ac.uk>
2020-07-21 10:53:58 +0100

    Update Vacancy class

--- src/main/java/ebe/DBClasses/Vacancy.java
+++ src/main/java/ebe/DBClasses/Vacancy.java
@@ -7,87 +7,42 @@ import java.util.Date;
 
 public class Vacancy {
 
-    private int vacancyID;
-    private int employerID;
-    private String position;
-    private @DateTimeFormat(pattern = "yyyy-MM-dd") Date applicationDeadline;
-    private double salary;
-    private String description;
-    private String typeOfContract;
-    private @DateTimeFormat(pattern = "HH:mm") Time duration;
+    private int VacancyID;
+    private int EmployerID;
+    private String Details;
+    private String Link;
+    private int  TypeOfVacancy;
+    private int StatusOfVacancy;
+    private @DateTimeFormat(pattern = "yyyy-MM-dd") Date StartOfVacancy;
+    private @DateTimeFormat(pattern = "yyyy-MM-dd") Date ClosingDate;
+    private int OcupationalCode;
+    private String ApplicationMethod;
+    private String Postcode;
 
-    public Vacancy(int vacancyID, int employerID, String position, Date applicationDeadline, double salary, String description, String typeOfContract, Time duration) {
-        this.vacancyID = vacancyID;
-        this.employerID = employerID;
-        this.position = position;
-        this.applicationDeadline = applicationDeadline;
-        this.salary = salary;
-        this.description = description;
-        this.typeOfContract = typeOfContract;
-        this.duration = duration;
+    public Vacancy(int vacancyID, int employerID, String details, String link, int typeOfVacancy, int statusOfVacancy, Date startOfVacancy, Date closingDate, int ocupationalCode, String applicationMethod, String postcode) {
+        VacancyID = vacancyID;
+        EmployerID = employerID;
+        Details = details;
+        Link = link;
+        TypeOfVacancy = typeOfVacancy;
+        StatusOfVacancy = statusOfVacancy;
+        StartOfVacancy = startOfVacancy;
+        ClosingDate = closingDate;
+        OcupationalCode = ocupationalCode;
+        ApplicationMethod = applicationMethod;
+        Postcode = postcode;
     }
 
-    public int getVacancyID() {
-        return vacancyID;
-    }
-
-    public void setVacancyID(int vacancyID) {
-        this.vacancyID = vacancyID;
-    }
-
-    public int getEmployerID() {
-        return employerID;
-    }
-
-    public void setEmployerID(int employerID) {
-        this.employerID = employerID;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public Date getApplicationDeadline() {
-        return applicationDeadline;
-    }
-
-    public void setApplicationDeadline(Date applicationDeadline) {
-        this.applicationDeadline = applicationDeadline;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTypeOfContract() {
-        return typeOfContract;
-    }
-
-    public void setTypeOfContract(String typeOfContract) {
-        this.typeOfContract = typeOfContract;
-    }
-
-    public Time getDuration() {
-        return duration;
-    }
-
-    public void setDuration(Time duration) {
-        this.duration = duration;
+    public Vacancy(int employerID, String details, String link, int typeOfVacancy, int statusOfVacancy, Date startOfVacancy, Date closingDate, int ocupationalCode, String applicationMethod, String postcode) {
+        EmployerID = employerID;
+        Details = details;
+        Link = link;
+        TypeOfVacancy = typeOfVacancy;
+        StatusOfVacancy = statusOfVacancy;
+        StartOfVacancy = startOfVacancy;
+        ClosingDate = closingDate;
+        OcupationalCode = ocupationalCode;
+        ApplicationMethod = applicationMethod;
+        Postcode = postcode;
     }
 }
