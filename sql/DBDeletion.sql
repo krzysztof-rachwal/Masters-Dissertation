bb21fffb466b33542864584f444931b1b6817432
Daniel <potkanskid@cardiff.ac.uk>
2020-07-22 08:47:12 +0100

    Add DB creation and population SQL queries

---
+++ sql/DBDeletion.sql
@@ -0,0 +1,28 @@
+DROP TABLE INT_AlumniWorkingForEmployer;
+DROP TABLE INT_AttendingEmployerOnEvent;
+DROP TABLE INT_AttendingSchoolsOnEvent;
+DROP TABLE INT_EmployerIndustrySector;
+DROP TABLE INT_EmployerSchoolPreferences;
+DROP TABLE INT_EmployerSupportOfAreasOfCurrriculum;
+DROP TABLE INT_LanguageUsedByEmployer;
+DROP TABLE INT_LocalAuthorityEmployerCanWorkWith;
+
+DROP TABLE EmployerDocumentLinks;
+DROP TABLE Alumni;
+DROP TABLE School;
+DROP TABLE Vacancy;
+DROP TABLE Event;
+DROP TABLE Employer;
+
+DROP TABLE EmployeesRangeList;
+DROP TABLE AreasOfCurriculumList;
+DROP TABLE LocalAuthorityList;
+DROP TABLE StatusOfEmployerList;
+DROP TABLE IndustrySectorList;
+DROP TABLE LanguagesList;
+DROP TABLE OccupationalCodesList;
+DROP TABLE TypeOfVacancyList;
+DROP TABLE StatusOfVacancyList;
+DROP TABLE TypeOfEventList;
+
+
