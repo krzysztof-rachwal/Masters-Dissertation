e01eef878e6bbc554874e4dbc9f797f5a0ccb044
Daniel <potkanskid@cardiff.ac.uk>
2020-07-22 08:51:37 +0100

    Add DB creation and population SQL queries

---
+++ sql/PopulateEmployeesRangeList.sql
@@ -0,0 +1,5 @@
+INSERT INTO EmployeesRangeList VALUES (1, "1-9");
+INSERT INTO EmployeesRangeList VALUES (2, "10-49");
+INSERT INTO EmployeesRangeList VALUES (3, "50-199");
+INSERT INTO EmployeesRangeList VALUES (4, "200-500");
+INSERT INTO EmployeesRangeList VALUES (5, "+500");
---
+++ sql/PopulateStatusOfEmployerList.sql
@@ -0,0 +1,3 @@
+INSERT INTO StatusOfEmployerList VALUES (1, "Live");
+INSERT INTO StatusOfEmployerList VALUES (2, "Closed down");
+INSERT INTO StatusOfEmployerList VALUES (3, "Not interested");
