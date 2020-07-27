1eb0fa6c7365b35059b113f8eadef60b809156c6
Daniel Leite <leiteDF@cardiff.ac.uk>
2020-07-27 10:02:45 +0100

    Update VacancyAPI

--- src/main/java/ebe/API/VacancyAPI.java
+++ src/main/java/ebe/API/VacancyAPI.java
@@ -1,13 +1,17 @@
 package ebe.API;
 
+import ebe.DBClasses.Vacancy;
 import ebe.DBMethods.EmployerQueries;
 import ebe.DBMethods.EventQueries;
 import ebe.DBMethods.SchoolQueries;
 import ebe.DBMethods.VacancyQueries;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 public class VacancyAPI {
@@ -22,6 +26,39 @@ public class VacancyAPI {
         EventQrys = ev;
         SchoolQrys = sc;
         VacancyQrys = va;
+    }
+
+    ///////////////////////    CREATE     ////////////////////////////////
+    //1. Create Vacancies
+    @RequestMapping(value="/api/create/vacancy", method= RequestMethod.GET)
+    public boolean createVacancy(
+            @RequestParam(name="employerName") String employerName,
+            @RequestParam(name="VacancyTitle") String vacancyTitle,
+            @RequestParam(name="Details") String details,
+            @RequestParam(name="Link") String link,
+            @RequestParam(name="TypeOfVacancy") int typeOfVacancy,
+            @RequestParam(name="StartOfVacancy") String startOfVacancy,
+            @RequestParam(name="ClosingDate") String closingDate,
+            @RequestParam(name="OccupationalCode") int occupationalCode,
+            @RequestParam(name="ApplicationMethod") String applicationMethod,
+            @RequestParam(name="Postcode") String postCode) throws ParseException {
+
+        System.out.println("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
+        int employerId = EmployerQrys.getEmployerIdByGivingName(employerName);
+        int StatusOfVacancy = 1;
+        System.out.println("P3");
+//        Date startOfVacancy = new SimpleDateFormat("yyyy/MM/dd").parse(startOfVacancyString);
+//      Date closingDate = new SimpleDateFormat("yyyy/MM/dd").parse(closingDateString);
+
+        System.out.println("-------------------------------------------");
+        System.out.println(employerId+vacancyTitle+details+link+typeOfVacancy+1+
+                startOfVacancy+closingDate+occupationalCode+applicationMethod+postCode);
+        // Status of Vacancy 1 = Live
+
+
+        return 1 == VacancyQrys.createVacancy(employerId,vacancyTitle,details,link,typeOfVacancy,StatusOfVacancy,
+                startOfVacancy,closingDate,occupationalCode,applicationMethod,postCode);
+
     }
 
     ///////////////////////    DELETE     ////////////////////////////////
