3d3a49a9f7bfbfc75bc8245b767e1d025831dfa6
Daniel Leite <leiteDF@cardiff.ac.uk>
2020-07-28 07:06:32 +0100

    Add create Event Ajax Request in events.js

--- src/main/resources/static/js/events.js
+++ src/main/resources/static/js/events.js
@@ -1,5 +1,51 @@
 
-//HTTP REQUEST METHODS
+//1. Create Event
+function createEvent() {
+    var baseUri = "/api/create/event";
+    var eventName = "eventName=" + $('input[id=event-name]').val();
+    var adviserName_url = "adviserName=" + $('input[id=adviser-name]').val();
+    var eventType_url = "eventType=" + $('select[id=event-type]').val();
+    var details_url = "Details=" + $('textarea[id=vacancy-desc]').val();
+    var eventDate_url = "TypeOfVeventDateacancy=" + $('input[id=event-date]').val();
+    var employerAttending_url="StatusOfVacancy=" + $('select[id=employers-attending]').val();
+    var eventTime_url = "StartOfVacancy=" + $('input[id=event-time]').val();
+    var schoolAttending_url = "ClosingDate=" + $('select[id=schools-attending]').val();
+    var eventVenue_url = "OccupationalCode=" + $('select[id=event-venue]').val();
+    var applicationMethod_url = "ApplicationMethod=" + $('select[id=appl-method]').val();
+    var postcode_url = "Postcode=" + $('input[id=post-code]').val();
+
+    var fullUri = baseUri + "?" + "&" + eventName+ "&" + adviserName_url + "&" + eventType_url
+        + "&" + eventType_url + "&" + eventDate_url  + "&" + "&" + employerAttending_url  + "&" + eventTime_url + "&"
+        + schoolAttending_url + "&" + eventVenue_url+ "&" + applicationMethod_url + "&" + postcode_url  ;
+
+    var token = $("meta[name='_csrf']").attr("content");    // Used to bypass Spring Boot's CSRF protocol     -- SOlution taken from 'https://stackoverflow.com/questions/34747437/use-of-spring-csrf-with-ajax-rest-call-and-html-page-with-thymeleaf' on Nov 26th 2019
+    var header = $("meta[name='_csrf_header']").attr("content");    // Used to bypass Spring Boot's CSRF protocol
+    console.log(fullUri)
+
+    $.ajax({
+        type: "GET", url: fullUri,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader(header, token);
+        },
+        success: function (data) {
+            if (data === true) {
+                location.assign("/vacancies")
+            } else {
+                alert("There was an error, please try again.")
+                alert(data.responseText)
+                alert(data)
+            }
+        },
+        error: function (data) {
+            alert("FAIL");
+            alert(data.responseText);
+            alert(data.toString());
+        }
+    });
+
+};
+
+// 2.Delete Event
 function deleteEvent(eventId) {
     var baseUri = "/api/delete/event";
     var eventId_url = "eventId=" + eventId;
@@ -30,3 +76,7 @@ function deleteEvent(eventId) {
 
     });
 }
+
+
+// For the Event Name selector
+$('.selectpicker').selectpicker();
