b37b08ccd1d53db143aa82ac6e7d5eea41d03deb
Kris <RachwalK@cardiff.ac.uk>
2020-08-01 20:36:51 +0100

    Created controller for email sender.

---
+++ src/main/java/ebe/Controllers/EmailController.java
@@ -0,0 +1,20 @@
+package ebe.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmailController {
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping(value = "/sendmail")
+    public String sendmail() {
+
+        emailService.sendMail("kate@example.com", "Test Subject", "Test mail");
+
+        return "emailsent";
+    }
+}
+}
