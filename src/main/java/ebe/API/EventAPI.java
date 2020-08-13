2f5d8efbc0c01f823fa8a66ba45d72a1915392b0
Daniel Leite <leiteDF@cardiff.ac.uk>
2020-08-13 12:43:44 +0100

    Fix issues with create/update events

--- src/main/java/ebe/API/EventAPI.java
+++ src/main/java/ebe/API/EventAPI.java
@@ -42,7 +42,7 @@ public class EventAPI {
             @RequestParam(name="eventPostcode") String EventPostcode,
             @RequestParam(name="eventSummary") String EventSummary,
             @RequestParam(name="isPublic") Boolean IsPublic,
-//          @RequestParam(name="isCancelled") Boolean isCancelled,
+            @RequestParam(name="isCancelled") Boolean isCancelled,
             @RequestParam(name="nameOfAdviser") String NameOfAdviser,
             @RequestParam(name="numberOfAttendees") int NumberOfAttendees,
             @RequestParam(name="promotesApprenticeships") Boolean PromotesApprenticeships,
@@ -52,7 +52,6 @@ public class EventAPI {
             @RequestParam(name="schoolAttending") String SchoolAttending) throws ParseException {
 
         //All events created start as not cancelled
-        boolean isCancelled = false;
         String EventDateAndTime = EventDate + " " + EventTime;
         ArrayList<Integer> employerIdList = new ArrayList<Integer>();
         ArrayList<Integer> schoolIdList = new ArrayList<Integer>();
@@ -73,15 +72,68 @@ public class EventAPI {
         int eventId = EventQrys.getLastEventCreated(EventName);
 
         //      Insert into the Employer / Event intersection table
-        EventQrys.updateEmployerEventIntersection(eventId,employerIdList);
+        EventQrys.createEmployerEventIntersection(eventId,employerIdList);
 
         //      Insert into the School / Event intersection table
-        EventQrys.updateSchoolEventIntersection(eventId, schoolIdList);
+        EventQrys.createSchoolEventIntersection(eventId, schoolIdList);
+
+        return true;
+    }
+
+
+    ///////////////////////    UPDATE     ////////////////////////////////
+    //2. Update Events
+    @RequestMapping(value="/api/update/event", method= RequestMethod.GET)
+    public Boolean updateEvent(
+            @RequestParam(name="eventID") int EventID,
+            @RequestParam(name="eventName") String EventName,
+            @RequestParam(name="typeOfEventID") int TypeOfEventID,
+            @RequestParam(name="eventDate") String EventDate,
+            @RequestParam(name="eventTime") String EventTime,
+            @RequestParam(name="eventVenueName") String EventVenueName,
+            @RequestParam(name="eventAddressCity") String EventAddressCity,
+            @RequestParam(name="eventAddressStreet") String EventAddressStreet,
+            @RequestParam(name="eventAddressNumber") String EventAddressNumber,
+            @RequestParam(name="eventPostcode") String EventPostcode,
+            @RequestParam(name="eventSummary") String EventSummary,
+            @RequestParam(name="isPublic") Boolean IsPublic,
+            @RequestParam(name="isCancelled") Boolean isCancelled,
+            @RequestParam(name="nameOfAdviser") String NameOfAdviser,
+            @RequestParam(name="numberOfAttendees") int NumberOfAttendees,
+            @RequestParam(name="promotesApprenticeships") Boolean PromotesApprenticeships,
+            @RequestParam(name="promotesWelshLanguage") Boolean PromotesWelshLanguage,
+            @RequestParam(name="challengesGenderStereotypes") Boolean ChallengesGenderStereotypes,
+            @RequestParam(name="employerAttending") String EmployerAttending,
+            @RequestParam(name="schoolAttending") String SchoolAttending) throws ParseException {
+
+        //All events created start as not cancelled
+        String EventDateAndTime = EventDate + " " + EventTime;
+        ArrayList<Integer> employerIdList = new ArrayList<Integer>();
+        ArrayList<Integer> schoolIdList = new ArrayList<Integer>();
+
+        for (String employerID : EmployerAttending.split(",")) {
+            employerIdList.add(Integer.parseInt(employerID));
+        }
+        for (String schoolID : SchoolAttending.split(",")) {
+            schoolIdList.add(Integer.parseInt(schoolID));
+        }
+
+        //Update the Event
+        EventQrys.updateEvent(EventID, EventName, TypeOfEventID, EventDateAndTime,  EventVenueName,  EventAddressCity,
+                EventAddressStreet, EventAddressNumber, EventPostcode, EventSummary, IsPublic, isCancelled, NameOfAdviser,
+                NumberOfAttendees,  PromotesApprenticeships,  PromotesWelshLanguage,ChallengesGenderStereotypes);
+
+//
+        //      Insert into the Employer / Event intersection table
+        EventQrys.updateEmployerEventIntersection(EventID,employerIdList);
+
+        //      Insert into the School / Event intersection table
+        EventQrys.updateSchoolEventIntersection(EventID, schoolIdList);
 
         return true;
     }
     ///////////////////////    DELETE     ////////////////////////////////
-    //2. Delete Events
+    //3. Delete Events
     @DeleteMapping("api/delete/event")
     public boolean deleteEvents(@RequestParam(value="eventId") Integer eventId){
 
