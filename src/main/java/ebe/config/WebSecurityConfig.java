819a5210da8bcc4d96dba555c5b8ac4d1fe7a1ec
Krzysztof Rachwal <rachwalk@cardiff.ac.uk>
2020-08-25 19:09:37 +0100

    Update WebSecurityConfig.java

--- src/main/java/ebe/config/WebSecurityConfig.java
+++ src/main/java/ebe/config/WebSecurityConfig.java
@@ -19,21 +19,24 @@ import javax.sql.DataSource;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
-    protected void configure( HttpSecurity http ) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception{
+    http.authorizeRequests().antMatchers("/", "/home", "/js/**", "/css/**").permitAll();
 
-        http.authorizeRequests()
-                .antMatchers( "/oauth2/**", "/login/**" ).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2Login()
-                .defaultSuccessUrl("/");
+    // protected void configure( HttpSecurity http ) throws Exception {
+
+    //     http.authorizeRequests()
+    //             .antMatchers( "/oauth2/**", "/login/**" ).permitAll()
+    //             .anyRequest().authenticated()
+    //             .and()
+    //             .oauth2Login()
+    //             .defaultSuccessUrl("/");
 
     }
 
-//    @Bean
-//    public AuthenticationSuccessHandler authSuccessHandler(){
-//        return new AuthSuccessHandler();
-//    }
+    @Bean
+    public AuthenticationSuccessHandler authSuccessHandler(){
+        return new AuthSuccessHandler();
+    }
 }
 
 
