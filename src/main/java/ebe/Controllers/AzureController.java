2b3500aa039206616cf32adfb2d08632a648a18d
Daniel <potkanskid@cardiff.ac.uk>
2020-08-26 09:03:04 +0100

    tech commit

---
+++ src/main/java/ebe/Controllers/AzureController.java
@@ -0,0 +1,44 @@
+package ebe.Controllers;
+
+import ebe.AzureBlobAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class AzureController {
+
+    @Autowired
+    private AzureBlobAdapter azureBlobAdapter;
+
+    @GetMapping("/container")
+    public ResponseEntity createContainer(@RequestParam String containerName){
+        System.out.println("JESTEM");
+        boolean created = azureBlobAdapter.createContainer(containerName);
+        return ResponseEntity.ok(created);
+    }
+
+    @GetMapping("/upload")
+    public ResponseEntity upload(@RequestParam MultipartFile multipartFile){
+        URI url = azureBlobAdapter.upload(multipartFile);
+        return ResponseEntity.ok(url);
+    }
+
+    @GetMapping("/blobs")
+    public ResponseEntity getAllBlobs(@RequestParam String containerName){
+        List<URI> uris = azureBlobAdapter.listBlobs(containerName);
+        return ResponseEntity.ok(uris);
+    }
+
+    @DeleteMapping
+    public ResponseEntity delete(@RequestParam String containerName, @RequestParam String blobName){
+        azureBlobAdapter.deleteBlob(containerName, blobName);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
