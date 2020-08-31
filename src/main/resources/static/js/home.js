2303de1c9e333060f7020d4f06ab1213aae04c08
Nikolina Antoniou <antonioun2@cardiff.ac.uk>
2020-08-31 20:13:13 +0100

    Changed redirect so it stays on the same page after submitting the request instead of redirecting to home.

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -2,10 +2,22 @@
 $(document).ready(function () {
 
     //1.1.  Feedback - Success
-    EventProfileShowInterest = localStorage.getItem("EventProfileShowInterest");
+    eventProfileShowInterest = localStorage.getItem("EventProfileShowInterest");
 
-    if (EventProfileShowInterest === "true"){
+    if (eventProfileShowInterest === "true"){
         $('#success_message_text').text(' The Interest in the event was sent!');
+        $('#success_message').removeClass('d-none').addClass('show');
+        localStorage.clear()
+        $("#success_message").fadeTo(1500, 1);
+        setTimeout(function(){
+            $("#success_message").fadeTo(1500, 0);
+        },5000);
+    }
+
+    requestEmployerEmail = localStorage.getItem("requestEmployerEmail");
+
+    if (requestEmployerEmail === "true"){
+        $('#success_message_text').text('You have successfully request an event with this employer.');
         $('#success_message').removeClass('d-none').addClass('show');
         localStorage.clear()
         $("#success_message").fadeTo(1500, 1);
