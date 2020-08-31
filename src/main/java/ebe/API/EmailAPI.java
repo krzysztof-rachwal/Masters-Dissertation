5602bd04d8e28ff157ab74a02b7e37f5e7bd6d28
Krzysztof Rachwal <RachwalK@cardiff.ac.uk>
2020-08-31 23:33:40 +0200

    extended html email service to all features requiring email. Improved email template.

--- src/main/java/ebe/API/EmailAPI.java
+++ src/main/java/ebe/API/EmailAPI.java
@@ -23,116 +23,118 @@ public class EmailAPI {
 
     private JavaMailSender javaMailSender;
 
+    String cwEmail = "carrers.wales@gmail.com";
+
     public EmailAPI(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
-    public void sendMail(String name, String fromEmail, String localAuth, String subject, String message) {
+//    public void sendMail(String name, String fromEmail, String localAuth, String subject, String message) {
+//
+//        var mailMessage = new SimpleMailMessage();
+//
+//        mailMessage.setTo(cwEmail);
+//        mailMessage.setSubject(subject);
+//        mailMessage.setText("You have a new message from " + name + ". Local Authority: " + localAuth + ". \nContent of the message: \n" + message);
+//        mailMessage.setFrom(fromEmail);
+//
+//        javaMailSender.send(mailMessage);
+//
+//
+//    }
 
-        var mailMessage = new SimpleMailMessage();
+//    public void sendRequestByEvent(String emailFrom, String schoolName, String localAuth, String eventName, String eventDate, String eventTime, String eventNotes, String eventType, String guests){
+//        var mailMessage = new SimpleMailMessage();
+//
+//        mailMessage.setTo("carrers.wales@gmail.com");
+//        mailMessage.setSubject("Request for " + eventName);
+//        mailMessage.setText("You have a new request for " + eventName +
+//                "\nSchool Name: " + schoolName +
+//                "\nSchool Local Authority: " + localAuth +
+//                "\nDate: " + eventDate +
+//                "\nTime: " + eventTime +
+//                "\nEvent type: " + eventType +
+//                "\nAdditional notes: " + eventNotes +
+//                "\nProposed guests: " + guests);
+//        mailMessage.setFrom(emailFrom);
+//        System.out.println(mailMessage);
+//
+//        javaMailSender.send(mailMessage);
+//        System.out.println("sent");
+//    }
 
-        mailMessage.setTo("carrers.wales@gmail.com");
-        mailMessage.setSubject(subject);
-        mailMessage.setText("You have a new message from " + name + ". Local Authority: " + localAuth + ". \nContent of the message: \n" + message);
-        mailMessage.setFrom(fromEmail);
+//    public void sendRequestByIndustry(String emailFrom, String schoolName, String localAuth, String eventName, String eventDate, String eventTime, String eventNotes, String eventType, String industry){
+//        var mailMessage = new SimpleMailMessage();
+//
+//        mailMessage.setTo("carrers.wales@gmail.com");
+//        mailMessage.setSubject("Request for " + eventName);
+//        mailMessage.setText("You have a new request for " + eventName +
+//                "\nSchool Name: " + schoolName +
+//                "\nSchool Local Authority: " + localAuth +
+//                "\nDate: " + eventDate +
+//                "\nTime: " + eventTime +
+//                "\nEvent type: " + eventType +
+//                "\nAdditional notes: " + eventNotes +
+//                "\nProposed Industry type(s): " + industry);
+//        mailMessage.setFrom(emailFrom);
+//
+//        javaMailSender.send(mailMessage);
+//    }
 
-        javaMailSender.send(mailMessage);
+//    public void sendRequestByLanguage(String emailFrom, String schoolName, String schoolPostcode, String eventName, String eventDate, String eventTime, String eventNotes, String eventType, String language){
+//        var mailMessage = new SimpleMailMessage();
+//
+//        mailMessage.setTo("carrers.wales@gmail.com");
+//        mailMessage.setSubject("Request for " + eventName);
+//        mailMessage.setText("You have a new request for " + eventName +
+//                "\nSchool Name: " + schoolName +
+//                "\nSchool Local Authority: " + schoolPostcode +
+//                "\nDate: " + eventDate +
+//                "\nTime: " + eventTime +
+//                "\nEvent type: " + eventType +
+//                "\nAdditional notes: " + eventNotes +
+//                "\nProposed language(s): " + language);
+//        mailMessage.setFrom(emailFrom);
+//
+//        javaMailSender.send(mailMessage);
+//    }
 
+//    public void sendShowInterest(String emailFrom, String schoolName, String localAuth, String eventName, String eventDate, String eventTime, String eventNotes){
+//        var mailMessage = new SimpleMailMessage();
+//
+//        mailMessage.setTo("carrers.wales@gmail.com");
+//        mailMessage.setSubject("Show Interest for " + eventName);
+//        mailMessage.setText("You have a new request for " + eventName +
+//                "\nSchool Name: " + schoolName +
+//                "\nSchool Local Authority: " + localAuth +
+//                "\nDate: " + eventDate +
+//                "\nTime: " + eventTime +
+//                "\nAdditional notes: " + eventNotes);
+//        mailMessage.setFrom(emailFrom);
+//
+//        javaMailSender.send(mailMessage);
+//    }
 
-    }
-
-    public void sendRequestByEvent(String emailFrom, String schoolName, String localAuth, String eventName, String eventDate, String eventTime, String eventNotes, String eventType, String guests){
-        var mailMessage = new SimpleMailMessage();
-
-        mailMessage.setTo("carrers.wales@gmail.com");
-        mailMessage.setSubject("Request for " + eventName);
-        mailMessage.setText("You have a new request for " + eventName +
-                "\nSchool Name: " + schoolName +
-                "\nSchool Local Authority: " + localAuth +
-                "\nDate: " + eventDate +
-                "\nTime: " + eventTime +
-                "\nEvent type: " + eventType +
-                "\nAdditional notes: " + eventNotes +
-                "\nProposed guests: " + guests);
-        mailMessage.setFrom(emailFrom);
-        System.out.println(mailMessage);
-
-        javaMailSender.send(mailMessage);
-        System.out.println("sent");
-    }
-
-    public void sendRequestByIndustry(String emailFrom, String schoolName, String localAuth, String eventName, String eventDate, String eventTime, String eventNotes, String eventType, String industry){
-        var mailMessage = new SimpleMailMessage();
-
-        mailMessage.setTo("carrers.wales@gmail.com");
-        mailMessage.setSubject("Request for " + eventName);
-        mailMessage.setText("You have a new request for " + eventName +
-                "\nSchool Name: " + schoolName +
-                "\nSchool Local Authority: " + localAuth +
-                "\nDate: " + eventDate +
-                "\nTime: " + eventTime +
-                "\nEvent type: " + eventType +
-                "\nAdditional notes: " + eventNotes +
-                "\nProposed Industry type(s): " + industry);
-        mailMessage.setFrom(emailFrom);
-
-        javaMailSender.send(mailMessage);
-    }
-
-    public void sendRequestByLanguage(String emailFrom, String schoolName, String schoolPostcode, String eventName, String eventDate, String eventTime, String eventNotes, String eventType, String language){
-        var mailMessage = new SimpleMailMessage();
-
-        mailMessage.setTo("carrers.wales@gmail.com");
-        mailMessage.setSubject("Request for " + eventName);
-        mailMessage.setText("You have a new request for " + eventName +
-                "\nSchool Name: " + schoolName +
-                "\nSchool Local Authority: " + schoolPostcode +
-                "\nDate: " + eventDate +
-                "\nTime: " + eventTime +
-                "\nEvent type: " + eventType +
-                "\nAdditional notes: " + eventNotes +
-                "\nProposed language(s): " + language);
-        mailMessage.setFrom(emailFrom);
-
-        javaMailSender.send(mailMessage);
-    }
-
-    public void sendShowInterest(String emailFrom, String schoolName, String localAuth, String eventName, String eventDate, String eventTime, String eventNotes){
-        var mailMessage = new SimpleMailMessage();
-
-        mailMessage.setTo("carrers.wales@gmail.com");
-        mailMessage.setSubject("Show Interest for " + eventName);
-        mailMessage.setText("You have a new request for " + eventName +
-                "\nSchool Name: " + schoolName +
-                "\nSchool Local Authority: " + localAuth +
-                "\nDate: " + eventDate +
-                "\nTime: " + eventTime +
-                "\nAdditional notes: " + eventNotes);
-        mailMessage.setFrom(emailFrom);
-
-        javaMailSender.send(mailMessage);
-    }
-
-    public void sendRequestEmployer(String fromEmail, String schoolName, String localAuth, String eventName,
-                                    String eventDate, String eventTime, String eventNotes, String eventType, String employerName){
-
-        var mailMessage = new SimpleMailMessage();
-
-
-        mailMessage.setTo("carrers.wales@gmail.com");
-        mailMessage.setSubject("Request event with employer: " + employerName);
-        mailMessage.setText("There is a new request for event " + eventName +
-                " with employer " + employerName + "." +
-                "\n\nSchool Name: " + schoolName +
-                "\nSchool Local Authority: " + localAuth +
-                "\nDate: " + eventDate +
-                "\nTime: " + eventTime +
-                "\nType: " + eventType +
-                "\nAdditional notes: " + eventNotes);
-        mailMessage.setFrom(fromEmail);
-
-        javaMailSender.send(mailMessage);
-    }
+//    public void sendRequestEmployer(String fromEmail, String schoolName, String localAuth, String eventName,
+//                                    String eventDate, String eventTime, String eventNotes, String eventType, String employerName){
+//
+//        var mailMessage = new SimpleMailMessage();
+//
+//
+//        mailMessage.setTo("carrers.wales@gmail.com");
+//        mailMessage.setSubject("Request event with employer: " + employerName);
+//        mailMessage.setText("There is a new request for event " + eventName +
+//                " with employer " + employerName + "." +
+//                "\n\nSchool Name: " + schoolName +
+//                "\nSchool Local Authority: " + localAuth +
+//                "\nDate: " + eventDate +
+//                "\nTime: " + eventTime +
+//                "\nType: " + eventType +
+//                "\nAdditional notes: " + eventNotes);
+//        mailMessage.setFrom(fromEmail);
+//
+//        javaMailSender.send(mailMessage);
+//    }
 
 
 
@@ -158,11 +160,140 @@ public class EmailAPI {
         context.setVariable("localAuth", localAuth);
         context.setVariable("messageBody", messageBody);
 
-        String html = templateEngine.process("email/emailTemplate", context);
+        String html = templateEngine.process("email/contactUsEmailTemplate", context);
 
-        helper.setTo("carrers.wales@gmail.com");
+        helper.setTo(cwEmail);
         helper.setText(html, true);
         helper.setSubject(subject);
+        helper.setFrom(fromEmail);
+
+        emailSender.send(message);
+    }
+
+    public void sendRequestByEvent(String emailFrom, String schoolName, String localAuth, String eventName, String eventDate, String eventTime, String eventNotes, String eventType, String guests) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        Context context = new Context();
+        context.setVariable("schoolName", schoolName);
+        context.setVariable("localAuth", localAuth);
+        context.setVariable("eventName", eventName);
+        context.setVariable("eventDate", eventDate);
+        context.setVariable("eventTime", eventTime);
+        context.setVariable("eventNotes", eventNotes);
+        context.setVariable("eventType", eventType);
+        context.setVariable("guests", guests);
+
+        String html = templateEngine.process("email/contactUsEmailTemplate", context);
+
+        helper.setTo(cwEmail);
+        helper.setText(html, true);
+        helper.setSubject("Request for " + eventName);
+        helper.setFrom(emailFrom);
+
+        emailSender.send(message);
+    }
+
+    public void sendRequestByIndustry(String emailFrom, String schoolName, String localAuth, String eventName, String eventDate, String eventTime, String eventNotes, String eventType, String industry) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        Context context = new Context();
+        context.setVariable("schoolName", schoolName);
+        context.setVariable("localAuth", localAuth);
+        context.setVariable("eventName", eventName);
+        context.setVariable("eventDate", eventDate);
+        context.setVariable("eventTime", eventTime);
+        context.setVariable("eventNotes", eventNotes);
+        context.setVariable("eventType", eventType);
+        context.setVariable("industry", industry);
+
+        String html = templateEngine.process("email/contactUsEmailTemplate", context);
+
+        helper.setTo(cwEmail);
+        helper.setText(html, true);
+        helper.setSubject("Request for " + eventName);
+        helper.setFrom(emailFrom);
+
+        emailSender.send(message);
+    }
+
+    public void sendRequestByLanguage(String emailFrom, String schoolName, String localAuth, String eventName, String eventDate, String eventTime, String eventNotes, String eventType, String language) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        Context context = new Context();
+        context.setVariable("schoolName", schoolName);
+        context.setVariable("localAuth", localAuth);
+        context.setVariable("eventName", eventName);
+        context.setVariable("eventDate", eventDate);
+        context.setVariable("eventTime", eventTime);
+        context.setVariable("eventNotes", eventNotes);
+        context.setVariable("eventType", eventType);
+        context.setVariable("guests", language);
+
+        String html = templateEngine.process("email/contactUsEmailTemplate", context);
+
+        helper.setTo(cwEmail);
+        helper.setText(html, true);
+        helper.setSubject("Request for " + eventName);
+        helper.setFrom(emailFrom);
+
+        emailSender.send(message);
+    }
+
+    public void sendShowInterest(String emailFrom, String schoolName, String localAuth, String eventName, String eventDate, String eventTime, String eventNotes) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        Context context = new Context();
+        context.setVariable("schoolName", schoolName);
+        context.setVariable("localAuth", localAuth);
+        context.setVariable("eventName", eventName);
+        context.setVariable("eventDate", eventDate);
+        context.setVariable("eventTime", eventTime);
+        context.setVariable("eventNotes", eventNotes);
+
+        String html = templateEngine.process("email/contactUsEmailTemplate", context);
+
+        helper.setTo(cwEmail);
+        helper.setText(html, true);
+        helper.setSubject("Request for " + eventName);
+        helper.setFrom(emailFrom);
+
+        emailSender.send(message);
+    }
+
+    public void sendRequestEmployer(String fromEmail, String schoolName, String localAuth, String eventName,
+                                    String eventDate, String eventTime, String eventNotes, String eventType, String employerName) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        Context context = new Context();
+        context.setVariable("schoolName", schoolName);
+        context.setVariable("localAuth", localAuth);
+        context.setVariable("eventName", eventName);
+        context.setVariable("eventDate", eventDate);
+        context.setVariable("eventTime", eventTime);
+        context.setVariable("eventNotes", eventNotes);
+        context.setVariable("eventType", eventType);
+        context.setVariable("employerName", employerName);
+
+        String html = templateEngine.process("email/contactUsEmailTemplate", context);
+
+        helper.setTo(cwEmail);
+        helper.setText(html, true);
+        helper.setSubject("Request for " + eventName);
         helper.setFrom(fromEmail);
 
         emailSender.send(message);
